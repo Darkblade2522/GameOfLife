ade96bd039115fa47ae6154b8fe49f9d4df73994
FYE <fyeblade@gmail.com>
2013-07-30 05:56:13 -0400

    Fix world size bug

--- gol.js
+++ gol.js
@@ -10,23 +10,27 @@ $(document).ready(function(){
     [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0],
      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+     
+     worldHeight = gameGrid.length,
+     worldWidth  = gameGrid[0].length;
     
     function computeNextGrid(currentGrid){
-        var nextGrid = currentGrid;
+        var nextGrid = [];
         
-        for (var i = 0 ; i < currentGrid.length -1; i++){
-            for (var j =0 ; j < currentGrid[i].length -1; j++){
+        for (var i = 0 ; i < worldHeight -1; i++){
+            nextGrid[i] = [];
+            for (var j = 0 ; j < worldWidth -1; j++){
                 //Count neighbours
-                var count = countNeighbours(currentGrid, i, j);
-                if (count != 0)
-                    console.log("x:" + i + "  y:" + j + "   count:"  +count);
+                var count = countNeighbours(currentGrid, j, i);
+                if (count !== 0)
+                    console.log("x:" + j + "  y:" + i + "   count:"  +count);
                     
                 //Decide action
                 switch (count) {
@@ -35,7 +39,7 @@ $(document).ready(function(){
                         nextGrid[i][j] = 0;
                         break;
                     case 2:
-                        nextGrid[i][j] = currentGrid[i][j];
+                        nextGrid[i][j] = (currentGrid[i][j] == 1 ? 1 : 0);
                         break;
                     case 3:
                         nextGrid[i][j] = 1;
@@ -57,22 +61,45 @@ $(document).ready(function(){
             for (var j = -1 ; j < 2 ; j++){
                 var x2 = x + i,
                     y2 = y + j;
-                if(i === 0  && j === 0){
-                    //continue;
+                    
+                //Check center
+                if(i === 0 && j === 0){
+                    continue;
                 }
-                
                 //Check borders
-                else if (grid[x2] === undefined || grid[x2][y2] === undefined){
-                    //continue;
+                if (grid[y2] === undefined || grid[y2][x2] === undefined){
+                //if (y2 < 0 || y2 >= grid.length || x2 < 0 || x2 >= grid[0].length){
+                    continue;
                 }
                 //Test if alive
-                else if (grid[x2][y2] == 1){
+                if (grid[y2][x2] == 1){
                     count++;
                 }
             }
         }
         return count;
     }
+    
+    /*function countNeighbours2(grid, x, y){
+        var count = 0,
+            pn = [[x-1, y-1], [x, y-1], [x+1, y-1], [x-1, y], [x+1, y], [x-1, y+1], [x, y+1], [x+1, y+1]]
+        x = parseInt(x);
+        y = parseInt(y);
+        for (var i = 0 ; i < 8 ; i++){
+            var x2 = pn[i][0],
+                y2 = pn[i][1];
+                
+            //Check borders
+            if (grid[y2] === undefined || grid[y2][x2] === undefined){
+                continue;
+            }
+            //Test if alive
+            if (grid[y2][x2] == 1){
+                count++;
+            }
+        }
+        return count;
+    }*/
     
     function displayGrid(grid){
         var HTMLgrid = "";
